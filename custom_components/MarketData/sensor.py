17c069d1e1c616ba418a2b3769b8caa242b59fd8
Rizqi Wijaya <rizwijaya@github.com>
2021-06-20 08:10:03 +0700

    Melakukan setting interpreter python dan hacs.json

---
+++ custom_components/MarketData/sensor.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""
+URL Market Data API
+https://api.nomics.com/v1/currencies/ticker?key=your-key-here&ids=BTC,ETH,XRP&interval=1d,30d&convert=EUR&per-page=100&page=1
+https://api.nomics.com/v1/currencies/ticker?key=kunci-nya&ids=pair&interval=time-Frame&per-page=100&page=1&sort=first_priced_at
+"""
